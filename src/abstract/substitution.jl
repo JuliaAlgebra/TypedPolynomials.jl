3687a718909cabbe4026d04d92f6cdf5d15ff71c
Robin Deits <robin.deits@gmail.com>
2017-04-26 15:47:48 -0400

    more tests

--- src/abstract/substitution.jl
+++ src/abstract/substitution.jl
@@ -49,6 +49,7 @@ forum: https://discourse.julialang.org/t/type-stable-zip-to-pairs/3390/2
 """
 pairzip(::Tuple{}, ::Tuple{}) = ()
 pairzip(::Tuple{}, ::Tuple) = throw(ArgumentError("args must be equal in length"))
+pairzip(::Tuple, ::Tuple{}) = throw(ArgumentError("args must be equal in length"))
 pairzip(t::Tuple, u::Tuple) = (t[1] => u[1], pairzip(Base.tail(t), Base.tail(u))...)
 pairzip(p::Pair{<:Tuple, <:Tuple}) = pairzip(p.first, p.second)
 
