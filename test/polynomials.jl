273579aa15d68524975e47a6e660202a4cfef517
Benoît Legat <benoit.legat@gmail.com>
2017-04-27 00:06:39 -0400

    Add broken tests @rdeits

--- test/polynomials.jl
+++ test/polynomials.jl
@@ -82,6 +82,8 @@ end
 
     @test @inferred(zero(t)) == @inferred(zero(typeof(t)))
     @test zero(t) == 0 * t
+    @test_broken @inferred(one(t)) == @inferred(one(typeof(t)))
+    @test_broken 1 == one(t) == 1 * t^0
 
     m1 = x * y * z^3 * x^2 * y
     m2 = x^3 * y^3 * z^2
@@ -98,10 +100,25 @@ end
     @test p.terms[2] == Term(x)
     @test variables(p) == (x,)
 
+    @test @inferred(zero(p)) == @inferred(zero(typeof(p)))
+    @test zero(p) == 0 * p
+    @test_broken @inferred(one(p)) == @inferred(one(typeof(p)))
+    @test_broken one(p) == 1
+
+    @test_broken mindeg(p) == 0
+    @test_broken maxdeg(p) == 1
+    @test_broken extdeg(p) == (0, 1)
+    @test_broken numvariables(p) == 1
+
     p = @inferred x^2 + x * x
     @test isa(p, Polynomial)
     @test length(terms(p)) == 1
     @test coefficient(terms(p)[1]) == 2
+
+    @test_broken mindeg(p) == 0
+    @test_broken maxdeg(p) == 2
+    @test_broken extdeg(p) == (0, 2)
+    @test_broken numvariables(p) == 1
 
     p = @inferred (1 + x) + (y + 2)
     @test coefficient(terms(p)[1]) == 3
@@ -112,10 +129,20 @@ end
     @test coefficient(terms(p)[3]) == 1
     @test exponents(terms(p)[3]) == (1, 0)
 
+    @test_broken mindeg(p) == 0
+    @test_broken maxdeg(p) == 1
+    @test_broken extdeg(p) == (0, 1)
+    @test_broken numvariables(p) == 2
+
     p = @inferred x^2 + y + x * x + 3 * x * y + x * y^2
     @test length(terms(p)) == 4
     @test coefficient.(terms(p)) == [1, 3, 2, 1]
     @test exponents.(terms(p)) == [(0, 1), (1, 1), (2, 0), (1, 2)]
+
+    @test_broken mindeg(p) == 1
+    @test_broken maxdeg(p) == 3
+    @test_broken extdeg(p) == (1, 3)
+    @test_broken numvariables(p) == 2
 
     @test (@wrappedallocs x^2 + y + x * x + 3 * x * y + x * y) <= 688
     @test (@wrappedallocs x^2 + 1) <= 128
