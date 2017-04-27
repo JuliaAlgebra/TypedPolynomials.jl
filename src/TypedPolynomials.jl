ce09c14268e92421d3740a76f6a5c86db1fefca9
Benoît Legat <benoit.legat@gmail.com>
2017-04-27 14:38:41 -0400

    Readd show

--- src/TypedPolynomials.jl
+++ src/TypedPolynomials.jl
@@ -30,6 +30,7 @@ import .Sequences: shortest_common_supersequence, mergesorted
 include("abstract/utils.jl")
 include("abstract/types.jl")
 include("abstract/operators.jl")
+include("abstract/show.jl")
 include("abstract/substitution.jl")
 include("abstract/calculus.jl")
 include("types.jl")
