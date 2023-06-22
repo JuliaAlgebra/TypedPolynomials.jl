37d46ed88609a8cb0614a670716a3ee127cf60d6
Benoît Legat <benoit.legat@gmail.com>
2023-06-22 12:25:02 +0200

    Improve promote_operation for substitution

--- src/substitution.jl
+++ src/substitution.jl
@@ -7,11 +7,18 @@ MP.substitute(st::MP.AbstractSubstitutionType, v::Variable, s::Substitution) = v
 MP.substitute(st::MP.AbstractSubstitutionType, v::Variable{Name}, s::Substitution{Name}, ::MP.AbstractSubstitution...) where {Name} = s.second
 
 _remove_variable(t::Tuple{}, ::Type) = t
-_remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{V}) where {V,N} = Base.tail(t)
-_remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{W}) where {V,W,N} = tuple(first(t), _remove_variable(Base.tail(t), V)...)
+function _remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{V}) where {V,N}
+    Base.tail(t)
+end
+function _remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{W}) where {V,W,N}
+    tuple(first(t), _remove_variable(Base.tail(t), W)...)
+end
+
+_mult_monomial_type(::Type{U}, ::Tuple{}) where {U} = U
+_mult_monomial_type(::Type{U}, V::Tuple) where {U} = MA.promote_operation(*, U, Monomial{V,length(V)})
 
 function MA.promote_operation(::typeof(MP.substitute), ::Type{MP.Subs}, ::Type{Monomial{V,N}}, ::Type{Pair{Variable{Name},T}}) where {V,N,Name,T}
-    U = MA.promote_operation(^, T, Int)
+    U = MA.promote_operation(*, T, T)
     VV = _remove_variable(V, Variable{Name})
-    return Term{U,Monomial{VV,length(VV)}}
+    return _mult_monomial_type(U, VV)
 end
