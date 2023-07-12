93552cc7a8991b4aa3f3368e4a044f04a392d471
Benoît Legat <benoit.legat@gmail.com>
2023-07-12 12:26:48 +0200

    Fix substitute promotion

--- src/substitution.jl
+++ src/substitution.jl
@@ -1,10 +1,59 @@
 const Substitution{Name} = Pair{<:Variable{Name}}
 
-## Variables
-MP.substitute(st::MP.AbstractSubstitutionType, v::Variable{Name}, s::Substitution{Name}) where {Name} = s.second
-MP.substitute(st::MP.AbstractSubstitutionType, v::Variable, s::Substitution) = v
-# subs(x, x=>y, y=>1) should be y, not 1 so as soon as we see the right variable, we stop, in subs(x, x=>y, x=>1), "x=>1" is ignored.
-MP.substitute(st::MP.AbstractSubstitutionType, v::Variable{Name}, s::Substitution{Name}, ::MP.AbstractSubstitution...) where {Name} = s.second
+function MA.promote_operation(
+    ::typeof(MP.substitute),
+    ::Type{<:MP.AbstractSubstitutionType},
+    ::Type{V},
+) where {V<:Variable}
+    return V
+end
+
+function MP.substitute(
+    ::MP.AbstractSubstitutionType,
+    v::Variable,
+)
+    return v
+end
+
+function MA.promote_operation(
+    ::typeof(MP.substitute),
+    ::Type{<:MP.AbstractSubstitutionType},
+    ::Type{Variable{Name}},
+    ::Type{Pair{Variable{Name},T}},
+    args::Vararg{Type,N}
+) where {Name,T,N}
+    return T
+end
+
+function MP.substitute(
+    ::MP.AbstractSubstitutionType,
+    ::Variable{Name},
+    s::Substitution{Name},
+    ::Vararg{MP.AbstractSubstitution,N},
+) where {Name,N}
+    return s.second
+end
+
+function MA.promote_operation(
+    ::typeof(MP.substitute),
+    S::Type{<:MP.AbstractSubstitutionType},
+    ::Type{V},
+    ::Type{<:Pair{<:Variable}},
+    args::Vararg{Type,N}
+) where {V<:Variable,N}
+    return MA.promote_operation(MP.substitute, S, V, args...)
+end
+
+function MP.substitute(
+    s::MP.AbstractSubstitutionType,
+    v::Variable,
+    ::Substitution,
+    args::Vararg{MP.AbstractSubstitution,N},
+) where {N}
+    return MP.substitute(s, v, args...)
+end
+
+# TODO remove below
 
 _remove_variable(::Tuple{}, ::Type) = nothing
 function _remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{V}) where {V,N}
@@ -20,9 +69,13 @@ function _remove_variable(t::Tuple{V,Vararg{Variable,N}}, ::Type{W}) where {V,W,
 end
 
 _mult_monomial_type(::Type{U}, ::Tuple{}) where {U} = U
-_mult_monomial_type(::Type{U}, V::Tuple) where {U} = MA.promote_operation(*, U, Monomial{V,length(V)})
+_mult_monomial_type(::Type{U}, V::Tuple) where {U} =
+    MA.promote_operation(*, U, Monomial{V,length(V)})
 
-function _promote_subs(::Type{Monomial{V,N}}, ::Type{Pair{Variable{Name},T}}) where {V,N,Name,T}
+function _promote_subs(
+    ::Type{Monomial{V,N}},
+    ::Type{Pair{Variable{Name},T}},
+) where {V,N,Name,T}
     U = MA.promote_operation(*, T, T)
     VV = _remove_variable(V, Variable{Name})
     if isnothing(VV)
@@ -33,10 +86,20 @@ function _promote_subs(::Type{Monomial{V,N}}, ::Type{Pair{Variable{Name},T}}) wh
     end
 end
 
-function MA.promote_operation(::typeof(MP.substitute), ::Type{MP.Subs}, ::Type{Monomial{V,N}}, ::Type{Pair{Variable{Name},T}}) where {V,N,Name,T}
+function MA.promote_operation(
+    ::typeof(MP.substitute),
+    ::Type{MP.Subs},
+    ::Type{Monomial{V,N}},
+    ::Type{Pair{Variable{Name},T}},
+) where {V,N,Name,T}
     return _promote_subs(Monomial{V,N}, Pair{Variable{Name},T})
 end
 
-function MA.promote_operation(::typeof(MP.substitute), ::Type{MP.Eval}, ::Type{Monomial{V,N}}, ::Type{Pair{Variable{Name},T}}) where {V,N,Name,T}
+function MA.promote_operation(
+    ::typeof(MP.substitute),
+    ::Type{MP.Eval},
+    ::Type{Monomial{V,N}},
+    ::Type{Pair{Variable{Name},T}},
+) where {V,N,Name,T}
     return _promote_subs(Monomial{V,N}, Pair{Variable{Name},T})
 end
